191027ba9da2e87cf79b2024dacb872bbdab206d
Rahman Mousavian <rmousavian@aconex.com>
2020-01-21 15:41:28 +1100

    convert port to int

--- config/production.py
+++ config/production.py
@@ -8,4 +8,4 @@ DBAAS_READONLY_CONNECTION_STRING = "oracle+cx_oracle://system:Sportal_DB#23@152.
 DBAAS_WRITEONLY_CONNECTION_STRING = "oracle+cx_oracle://system:Sportal_DB#23@140.238.248.176:1521/?service_name=PDB1.svcsubnetad1.svcvcn.oraclevcn.com"
 
 SERVER_ADDRESS = "0.0.0.0"
-SERVER_PORT = "8080"
+SERVER_PORT = 8080
