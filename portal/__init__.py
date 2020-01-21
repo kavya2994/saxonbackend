181bb9420b61b7856c235f687fea7d701fb86b15
Rahman Mousavian <rmousavian@aconex.com>
2020-01-21 15:35:49 +1100

    Change defualt port to 8080

--- portal/__init__.py
+++ portal/__init__.py
@@ -21,7 +21,10 @@ def create_app(config=None):
         elif config.endswith('.py'):
             app.config.from_pyfile(config)
 
-    http_server = WSGIServer(('0.0.0.0', 811), app)
+    server_address = app.config.get('SERVER_ADDRESS', default='0.0.0.0')
+    server_port = app.config.get('PORT', default='8080')
+
+    http_server = WSGIServer((server_address, server_port), app)
     http_server.serve_forever()
     return app
 
