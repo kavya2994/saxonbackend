27600ab7a29209980c4f8187579b449d0afce222
sushma-dantuluri <46386201+sushma-dantuluri@users.noreply.github.com>
2020-01-21 14:56:45 +1100

    Update production.py
    updated write DB connect string

--- config/production.py
+++ config/production.py
@@ -5,4 +5,4 @@ DEBUG = False
 DATA_DIR = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
 
 DBAAS_READONLY_CONNECTION_STRING = "oracle+cx_oracle://system:Sportal_DB#23@152.67.0.98:1521/?service_name=Dev_bom1sn.devpublic.dev.oraclevcn.com"
-# DBAAS_WRITEONLY_CONNECTION_STRING = ""
+DBAAS_WRITEONLY_CONNECTION_STRING = "oracle+cx_oracle://system:Sportal_DB#23@140.238.248.176:1521/?service_name=PDB1.svcsubnetad1.svcvcn.oraclevcn.com"
