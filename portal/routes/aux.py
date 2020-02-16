cfde844233b13d040c63dc1ea38808244de81771
Rahman Mousavian <rmousavian@aconex.com>
2020-02-16 17:41:10 +1100

    Aux cors fix

--- portal/routes/aux.py
+++ portal/routes/aux.py
@@ -5,7 +5,7 @@ from ..api import api
 from ..helpers import crossdomain, RESPONSE_OK
 
 
-@api.route("/my-ip", methods=["GET"])
+@api.route("/my-ip")
 @api.doc(description='Get user\'s IP address')
 class MyIP(Resource):
     @crossdomain(whitelist=APP.config['CORS_ORIGIN_WHITELIST'], headers=APP.config['CORS_HEADERS'])
@@ -13,12 +13,13 @@ class MyIP(Resource):
         pass
 
     @crossdomain(whitelist=APP.config['CORS_ORIGIN_WHITELIST'], headers=APP.config['CORS_HEADERS'])
+    @api.doc(description='Get user\'s IP address')
     def get(self):
         return {'ip': request.environ['REMOTE_ADDR']}
 
 
 
-@api.route("/status", methods=["GET"])
+@api.route("/status")
 @api.doc(description='Get Status of API Server')
 class Status(Resource):
     @crossdomain(whitelist=APP.config['CORS_ORIGIN_WHITELIST'], headers=APP.config['CORS_HEADERS'])
@@ -26,5 +27,6 @@ class Status(Resource):
         pass
 
     @crossdomain(whitelist=APP.config['CORS_ORIGIN_WHITELIST'], headers=APP.config['CORS_HEADERS'])
+    @api.doc(description='Get Status of API Server')
     def get(self):
         return RESPONSE_OK
