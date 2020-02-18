6914005a8aeb9a5fc26e4cde3f75908a1896f6db
venkatesh <venky.y1997@gmail.com>
2020-02-18 15:47:32 +0530

    18-02-2020

---
+++ portal/routes/forms/forms_queue.py
@@ -0,0 +1,45 @@
+import json
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from flask import Blueprint, jsonify, request
+from flask_restplus import Resource, reqparse, inputs
+from werkzeug.exceptions import NotFound, BadRequest, Unauthorized, UnprocessableEntity, InternalServerError
+from ...helpers import token_verify_or_raise, crossdomain, RESPONSE_OK
+from ...models import db, status, roles
+from ...models.enrollmentform import Enrollmentform
+from ...models.terminationform import Terminationform, TerminationformResponseModel
+from ...models.token import Token, TOKEN_FORMTYPE_TERMINATION
+from ...models.comments import Comments
+from ...models.roles import *
+from ...services.mail import send_email
+from . import ns
+from ... import APP
+
+parser = reqparse.RequestParser()
+parser.add_argument('Authorization', type=str, location='headers', required=True)
+parser.add_argument('username', type=str, location='headers', required=True)
+parser.add_argument('Ipaddress', type=str, location='headers', required=True)
+
+
+@ns.route("/initiate")
+class FormQueue(Resource):
+    @crossdomain(whitelist=APP.config['CORS_ORIGIN_WHITELIST'], headers=APP.config['CORS_HEADERS'])
+    def options(self):
+        pass
+
+    @crossdomain(whitelist=APP.config['CORS_ORIGIN_WHITELIST'], headers=APP.config['CORS_HEADERS'])
+    @ns.doc(parser=parser,
+            description='Initiate Termination',
+            responses={200: 'OK', 400: 'Bad Request', 401: 'Unauthorized', 500: 'Internal Server Error'})
+    @ns.expect(parser, validate=True)
+    @ns.marshal_with(TerminationformResponseModel)
+    def get(self):
+        args = parser.parse_args(strict=False)
+        token = token_verify_or_raise(token=args["Authorization"], user=args["username"], ip=args["Ipaddress"])
+        if token["role"] == roles.ROLES_REVIEW_MANAGER:
+            forms_data = []
+            db.session.query(Token, Enrollmentform).filter(Token.FormID == Enrollmentform.FormID,
+                                                           Token.FormStatus.in_([status.STATUS_PENDING, status.]))
+
+
