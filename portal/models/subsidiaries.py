2532942d92da6072d03a81091796f16e502b5bbb
venkatesh <venky.y1997@gmail.com>
2020-02-24 16:31:45 +0530

    added primary key subsidiary

--- portal/models/subsidiaries.py
+++ portal/models/subsidiaries.py
@@ -6,6 +6,7 @@ from sqlalchemy import Sequence
 class Subsidiaries(db.Model):
     __bind_key__ = 'writeonly'
 
+    TransID = db.Column(db.Integer, Sequence('trans_id_seq'), primary_key=True, nullable=False)
     SubsidiaryID = db.Column(db.String(255), nullable=False)
     SubsidiaryName = db.Column(db.String(255))
 
