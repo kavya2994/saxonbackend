4804cad6024d525bda3b4e269d54b81338fd7546
venkatesh <venky.y1997@gmail.com>
2020-02-24 16:27:20 +0530

    added primary key

--- portal/models/emp_mem_relation.py
+++ portal/models/emp_mem_relation.py
@@ -1,8 +1,11 @@
+from sqlalchemy import Sequence
+
 from . import db
 
 
 class EmpMemRelation(db.Model):
     __bind_key__ = 'writeonly'
+    EmpMemID = db.Column(db.Integer, Sequence('empmem_id_seq'), primary_key=True, nullable=False)
     EmployerID = db.Column(db.String(255), nullable=False),
     EmployerName = db.Column(db.String(255)),
     MemberID = db.Column(db.String(255), nullable=False),
