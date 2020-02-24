67345caf0edba0562a3750c42ff6929c992f64a4
venkatesh <venky.y1997@gmail.com>
2020-02-24 19:58:12 +0530

    test1

--- portal/routes/admin/get_employer_to_member.py
+++ portal/routes/admin/get_employer_to_member.py
@@ -57,8 +57,8 @@ class GetEmployerMemberRelation(Resource):
                 MemberView.EMPOYER == EmployerView.SNAME,
                 MemberView.MEMNO == member_id).all()
             if employers_from_view is not None:
-                for employers in employers_from_view:
-                    print(employers.__dict__)
+                for members, employers in employers_from_view:
+                    print(employers)
                     employers_data_list.append({
                         "EmployerNo": employers.ERNO,
                         "EmployerName": employers.ENAME
