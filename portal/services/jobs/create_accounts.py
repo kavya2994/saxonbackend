012e93153ee2b4813cfc1db376f4f7012af1c16a
venkatesh <venky.y1997@gmail.com>
2020-03-18 18:17:26 +0530

    create accounts fix

--- portal/services/jobs/create_accounts.py
+++ portal/services/jobs/create_accounts.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from threading import Thread
 
 from ..mail import send_email
 from ... import APP, LOG
@@ -12,27 +13,28 @@ from ...models.status import *
 from ...helpers import randomStringwithDigitsAndSymbols
 
 
-def send_temporary_passwords():
-    users = Users.query.filter(Users.Password.is_(None)).all()
-    for user in users:
-        try:
-            random_password = randomStringwithDigitsAndSymbols()
-            enc_random_pass = Encryption().encrypt("test")
-            user.Password = enc_random_pass
-            user.TemporaryPassword = True
-            user.UserCreatedTime = datetime.utcnow()
-            db.session.commit()
-        except Exception as e:
-            LOG.error(e)
-        # msg_text = f'<p>Dear {user.DisplayName}</p>' + \
-        #            f'<p>Your account has been created</p>' + \
-        #            f'<p>Username is {user.Username}</p>' + \
-        #            f'<p> please use this password {"test"} to log in</p>' + \
-        #            f'<p> Please ensure that you are not copying any extra spaces</p>' \
-        #            f'<p>Please use below link to login</p>' \
-        #            f'<p>{APP.config["FRONTEND_URL"]}/login</p>'
-        # if user.Email is not None:
-        #     send_email(user.Email, "Welcome to Pension Management portal", body=msg_text)
+def send_temporary_passwords(app):
+    with app.app_context():
+        users = Users.query.filter(Users.Password.is_(None)).all()
+        for user in users:
+            try:
+                random_password = randomStringwithDigitsAndSymbols()
+                enc_random_pass = Encryption().encrypt("test")
+                user.Password = enc_random_pass
+                user.TemporaryPassword = True
+                user.UserCreatedTime = datetime.utcnow()
+                db.session.commit()
+            except Exception as e:
+                LOG.error(e)
+            # msg_text = f'<p>Dear {user.DisplayName}</p>' + \
+            #            f'<p>Your account has been created</p>' + \
+            #            f'<p>Username is {user.Username}</p>' + \
+            #            f'<p> please use this password {"test"} to log in</p>' + \
+            #            f'<p> Please ensure that you are not copying any extra spaces</p>' \
+            #            f'<p>Please use below link to login</p>' \
+            #            f'<p>{APP.config["FRONTEND_URL"]}/login</p>'
+            # if user.Email is not None:
+            #     send_email(user.Email, "Welcome to Pension Management portal", body=msg_text)
 
 
 def create_accounts(app):
@@ -55,13 +57,13 @@ def create_accounts(app):
             except Exception as e:
                 LOG.error(e)
                 continue
-
+        Thread(target=send_temporary_passwords, args=(app,)).start()
         members = MemberView.query.all()
 
         for member in members:
             try:
-                user = Users(UserID=member.ERKEY,
-                             Username=member.ERNO,
+                user = Users(UserID=member.MKEY,
+                             Username=member.MEMNO,
                              Email=member.EMAIL,
                              DisplayName=member.FNAME if member.FNAME is not None else "" + " " + member.LNAME if member.LNAME is not None else "",
                              Role=ROLES_MEMBER,
@@ -71,4 +73,4 @@ def create_accounts(app):
             except Exception as e:
                 LOG.error(e)
                 continue
-        send_temporary_passwords()
+        Thread(target=send_temporary_passwords, args=(app,)).start()
